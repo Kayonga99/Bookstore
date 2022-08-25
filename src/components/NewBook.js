24dbd7f1304d55e21bf06a673e7ce6239dc1e001
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-25 23:08:29 +0200

    Update Newbook.js.

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -1,22 +1,4 @@
-import React from 'react';
-
-const AddNewBook = () => (
-  <div>
-    <h2>Add New Book</h2>
-    <input type="text" placeholder="Book title" />
-    <form action="#">
-      <select name="books" id="books">
-        <option value="">Catefory</option>
-        <option value="biography">Biography</option>
-        <option value="romance">Romance</option>
-        <option value="action">Action</option>
-        <option value="health">Health</option>
-
-      </select>
-
-      <button type="submit"> Add Book</button>
-    </form>
-  </div>
-);
-
-export default AddNewBook;
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
