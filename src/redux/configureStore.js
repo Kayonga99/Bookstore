c76b28a57a5edcaf7c318b83c4acafda58b5de59
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-26 00:26:50 +0200

    Import checkReducer.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
+import checkReducer from './categories/categories';
 
 const reducer = combineReducers({
   booksReducer,
+  checkReducer,
 });
 
 const store = createStore(
