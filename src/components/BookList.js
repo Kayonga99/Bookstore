2bcb92b6537c053483b5d15ab4827f911d8bf14d
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-27 11:10:58 +0200

    Update branch

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import BookItem from './BookItem';
-import BookAdd from './BookAdd';
+import BookAdd from './NewBook';
 
 function BookList() {
   return (
