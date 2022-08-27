ea0d2ae5c5ec24cffc007df8240779c4e6f8ef94
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-27 21:40:24 +0200

    Update categories.js and fix linter errors.

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
   const categories = useSelector((state) => state.checkReducer.categories);
-
+  const dispatch = useDispatch();
   return (
-
-    <div>
-      <h2>{categories}</h2>
+    <div className="underContruction">
+      <button type="button" onClick={() => dispatch(checkStatus())}>
+        Check status
+      </button>
+      <h1>{categories}</h1>
     </div>
   );
 };
